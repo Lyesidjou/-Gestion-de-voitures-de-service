c2b803dc446893da4435927429b90406fd8f86e8
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-03-01 16:35:33 -0500

    creation de la classe voiture avec ses attributs

--- Employe,voiture.py
+++ Employe,voiture.py
@@ -6,3 +6,10 @@ class Employe():
         self.voitureService= None
     def afficher_informations(self):
         print(f"l'Employe qui as le numero de permis {self.numeroPermis} est nomee {self.nom} {self.prenom}")
+class Voiture:
+    def __init__(self, matricule:str,annee:int, marque:str,kilometrage:int):
+        self.matricule = matricule
+        self.annee = annee
+        self.marque = marque
+        self.kilometrage = kilometrage
+        self.chauffeur = None
