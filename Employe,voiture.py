06572d1ed0a3eb3106792f5427ae97f40d083f49
Lyes Idjourdikane <lyes.idjourdikane@gmail.com>
2026-03-03 16:10:41 -0500

    creation de la methode affecter voiture avec la condition que si la voiture possede deja un chauffeur on ne peut pas lui attribuer un autre, et que si le chaffeur possede deja une voiture il ne peut pas avoir une autre

--- Employe,voiture.py
+++ Employe,voiture.py
@@ -3,4 +3,15 @@ class Employe():
         self.numeroPermis = numeroPermis
         self.nom = nom
         self.prenom = prenom
-        self.liste_voitures = []
+        self.voitureService = None
+    def affecter_voiture(self, voiture):
+        if self.voitureService==None:
+            if voiture.chauffeur==None:
+                self.voitureService = voiture
+                voiture.chauffeur = self
+            else:
+                print("la voiture est attribue deja a un chauffeur")
+        else:
+            print("le chauffeur a deja un vehicule de service")
+
+
